3970a695be73b1ac6486b5a7580343b3e502bf17
abdel <abdel@mosaic.fr>
2020-08-14 17:36:29 +0200

    404 css

--- src/template/notfound/NotFound.tsx
+++ src/template/notfound/NotFound.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
 const NotFound: React.FunctionComponent = () => (
-  <div>
-    Cette page n&#39;existe pas
+  <div className="error-page">
+    <div className="error-page__content">
+      <h1>404</h1>
+      <h4>Oups ! Page non trouvé</h4>
+      <p>Désolé, la page que vous visitez n&#39;existe pas. Si vous pensez que le site ne fonctionne pas, faite nous un retour.</p>
+      <div className="error-page__buttons">
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a href="#">retour a la page d&#39;accueil</a>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a href="#">signalez un prbolème</a>
+      </div>
+    </div>
   </div>
 );
 
